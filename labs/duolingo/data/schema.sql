6c1a8a868173bd3352e9c22b7f83b3517db84934
Aliaksei Charnukha <alex.a.chernukha@gmail.com>
2025-05-17 22:09:01 +0300

    Final database version

--- labs/duolingo/data/schema.sql
+++ labs/duolingo/data/schema.sql
@@ -67,3 +67,79 @@ WITH RECURSIVE cnt(x) AS (
     SELECT x + 1 FROM cnt WHERE x < 172
 )
 SELECT x FROM cnt;
+
+CREATE TABLE IF NOT EXISTS grammar_questions_easy
+(
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL,
+    options1  TEXT NOT NULL,
+    options2  TEXT NOT NULL,
+    options3  TEXT NOT NULL,
+    options4  TEXT NOT NULL,
+    answer INTEGER NOT NULL,
+    rule TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS grammar_questions_medium
+(
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL,
+    options1  TEXT NOT NULL,
+    options2  TEXT NOT NULL,
+    options3  TEXT NOT NULL,
+    options4  TEXT NOT NULL,
+    answer INTEGER NOT NULL,
+    rule TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS grammar_questions_hard
+(
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL,
+    options1  TEXT NOT NULL,
+    options2  TEXT NOT NULL,
+    options3  TEXT NOT NULL,
+    options4  TEXT NOT NULL,
+    answer INTEGER NOT NULL,
+    rule TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS unanswered_grammar_easy_questions
+(
+    question_id INTEGER PRIMARY KEY
+);
+
+INSERT INTO unanswered_grammar_easy_questions (question_id)
+WITH RECURSIVE cnt(x) AS (
+    SELECT 1
+    UNION ALL
+    SELECT x + 1 FROM cnt WHERE x < 150
+)
+SELECT x FROM cnt;
+
+CREATE TABLE IF NOT EXISTS unanswered_grammar_medium_questions
+(
+    question_id INTEGER PRIMARY KEY
+);
+
+INSERT INTO unanswered_grammar_medium_questions (question_id)
+WITH RECURSIVE cnt(x) AS (
+    SELECT 1
+    UNION ALL
+    SELECT x + 1 FROM cnt WHERE x < 100
+)
+SELECT x FROM cnt;
+
+CREATE TABLE IF NOT EXISTS unanswered_grammar_hard_questions
+(
+    question_id INTEGER PRIMARY KEY
+);
+
+INSERT INTO unanswered_grammar_hard_questions (question_id)
+WITH RECURSIVE cnt(x) AS (
+    SELECT 1
+    UNION ALL
+    SELECT x + 1 FROM cnt WHERE x < 90
+)
+SELECT x FROM cnt;
